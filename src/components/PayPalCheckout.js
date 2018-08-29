92143a0e7d646d0a7ea226f3f396bdb7b9ca99e6
flfk <flfk@users.noreply.github.com>
2018-08-29 13:48:39 -0300

    Basic PayPal Btn component

--- src/components/PayPalCheckout.js
+++ src/components/PayPalCheckout.js
@@ -1,63 +1,137 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import scriptLoader from 'react-async-script-loader';
+import paypal from 'paypal-checkout';
 
 const propTypes = {};
 
 const defaultProps = {};
 
-// class PayPalCheckout extends React.Component {
-//   state = {
-//     showButton = false;
-//   }
+class PaypalButton extends React.Component {
+  constructor(props) {
+    super(props);
 
-//   render() {
-//     return <div />;
-//   }
-// }
-
-// PayPalCheckout.propTypes = propTypes;
-// PayPalCheckout.defaultProps = defaultProps;
-
-// export default PayPalCheckout;
-
-let PayPalButton = paypal.Button.driver('react', { React, ReactDOM });
-
-class Main extends React.Component {
-  constructor() {
-    super();
     this.state = {
-      env: 'sandbox',
-      client: {
-        sandbox: 'AWi18rxt26-hrueMoPZ0tpGEOJnNT4QkiMQst9pYgaQNAfS1FLFxkxQuiaqRBj1vV5PmgHX_jA_c1ncL',
-        production: '<insert production client id>'
-      },
-      commit: true
+      showButton: false
     };
+
+    window.React = React;
+    window.ReactDOM = ReactDOM;
   }
-  payment(data, actions) {
-    return actions.payment.create({
-      transactions: [
-        {
-          amount: { total: '0.01', currency: 'USD' }
-        }
-      ]
-    });
+
+  componentDidMount() {
+    const { isScriptLoaded, isScriptLoadSucceed } = this.props;
+
+    if (isScriptLoaded && isScriptLoadSucceed) {
+      this.setState({ showButton: true });
+    }
   }
-  onAuthorize(data, actions) {
-    return actions.payment.execute().then(function(paymentData) {
-      // Show a success page to the buyer
-    });
+
+  componentWillRecieveProps(nextProps) {
+    const { isScriptLoaded, isScriptLoadSucceed } = nextProps;
+
+    const isLoadedPreviouslyWasnt =
+      !this.state.showButton && this.props.isScriptLoaded && isScriptLoaded;
+
+    if (isLoadedPreviouslyWasnt && isScriptLoadSucceed) {
+      this.setState({ showButton: true });
+    }
   }
+
   render() {
+    const { total, currency, env, commit, client, onSuccess, onError, onCancel } = this.props;
+
+    const { showButton } = this.state;
+
+    let PayPalButton = paypal.Button.driver('react', { React, ReactDOM });
+
+    const payment = () =>
+      paypal.rest.payment.create(env, client, {
+        transactions: [
+          {
+            amount: {
+              total,
+              currency
+            }
+          }
+        ]
+      });
+
+    const onAuthorize = (data, actions) =>
+      actions.payment.execute().then(() => {
+        const payment = {
+          paid: true,
+          cancelled: false,
+          payerID: data.payerID,
+          paymentID: data.paymentID,
+          paymentToken: data.paymentToken,
+          returnUrl: data.returnUrl
+        };
+
+        onSuccess(payment);
+      });
+
     return (
-      <PayPalButton
-        commit={this.state.commit}
-        env={this.state.env}
-        client={this.state.client}
-        payment={(data, actions) => this.payment(data, actions)}
-        onAuthorize={(data, actions) => this.onAuthorize(data, actions)}
-      />
+      <div>
+        {showButton && (
+          <PayPalButton
+            env={env}
+            client={client}
+            commit={commit}
+            payment={payment}
+            onAuthorize={onAuthorize}
+            onCancel={onCancel}
+            onError={onError}
+          />
+        )}
+      </div>
     );
   }
 }
+
+PaypalButton.propTypes = propTypes;
+PaypalButton.defaultProps = defaultProps;
+
+export default scriptLoader('https://www.paypalobjects.com/api/checkout.js')(PaypalButton);
+
+// let PayPalButton = paypal.Button.driver('react', { React, ReactDOM });
+
+// class Main extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       env: 'sandbox',
+//       client: {
+//         sandbox: 'AWi18rxt26-hrueMoPZ0tpGEOJnNT4QkiMQst9pYgaQNAfS1FLFxkxQuiaqRBj1vV5PmgHX_jA_c1ncL',
+//         production: '<insert production client id>'
+//       },
+//       commit: true
+//     };
+//   }
+//   payment(data, actions) {
+//     return actions.payment.create({
+//       transactions: [
+//         {
+//           amount: { total: '0.01', currency: 'USD' }
+//         }
+//       ]
+//     });
+//   }
+//   onAuthorize(data, actions) {
+//     return actions.payment.execute().then(function(paymentData) {
+//       // Show a success page to the buyer
+//     });
+//   }
+//   render() {
+//     return (
+//       <PayPalButton
+//         commit={this.state.commit}
+//         env={this.state.env}
+//         client={this.state.client}
+//         payment={(data, actions) => this.payment(data, actions)}
+//         onAuthorize={(data, actions) => this.onAuthorize(data, actions)}
+//       />
+//     );
+//   }
+// }
