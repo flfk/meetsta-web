1278114cb812d4f96835d4b68b5a4599e4f2e7e9
flfk <flfk@users.noreply.github.com>
2018-09-03 12:33:04 -0300

    Updated firebase initialisation

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -1,18 +1,21 @@
-import firebase from 'firebase';
-
-require('firebase/firestore');
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 const config = {
-  apiKey: 'AIzaSyDhdaMR1VQI3NnKdsMIaPnwSyeANeo8o6A',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: 'online-meet-and-greets.firebaseapp.com',
   databaseURL: 'https://online-meet-and-greets.firebaseio.com',
   projectId: 'online-meet-and-greets',
   storageBucket: 'online-meet-and-greets.appspot.com',
-  messagingSenderId: '309382713009'
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
 
 const db = firebase.firestore();
+
+// To ensure ensure firestore timestamp objects supported in future
+const settings = { timestampsInSnapshots: true };
+db.settings(settings);
 
 export default db;
