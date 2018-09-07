7c600620f0af564b5ea46c173ecece97b452c7b7
flfk <flfk@users.noreply.github.com>
2018-09-07 12:05:23 -0300

    Sendgrid template working

--- functions/index.js
+++ functions/index.js
@@ -7,48 +7,32 @@ admin.initializeApp(functions.config().firebase);
 const SENDGRID_API_KEY = functions.config().sendgrid.key;
 sgMail.setApiKey(SENDGRID_API_KEY);
 
-exports.firestoreOrderConfirmationEmail = functions.firestore
+exports.orderConfirmationEmail = functions.firestore
   .document('tickets/{ticketID}')
   .onCreate(snap => {
     const ticket = snap.data();
-
-    // const msg = {
-    //   to: 'felixlkramer@gmail.com',
-    //   from: 'contact.meetsta@gmail.com',
-    //   subject: 'Test Subject Line in Function',
-
-    //   templateID: 'd-9151449bb4e4476eb06436f9574a0a01',
-    //   substitutionWrappers: ['{{', '}}'],
-    //   substitutions: {
-    //     eventName: 'testEvent in Functions',
-    //     purchaseNameFirst: 'FunctionsName'
-    //   }
-    // };
-
-    // const msg = {
-    //   to: 'felixlkramer',
-    //   from: 'contact.meetsta@gmail.com',
-    //   subject: 'Testing',
-
-    //   // custom templates
-    //   templateId: 'd-9151449bb4e4476eb06436f9574a0a01',
-    //   substitutionWrappers: ['{{', '}}'],
-    //   substitutions: {
-    //     eventName: 'testEvent in Functions',
-    //     purchaseNameFirst: 'FunctionsName'
-    //   }
-    // };
 
     const msg = {
       to: 'felixlkramer@gmail.com',
       from: 'contact.meetsta@gmail.com',
       subject: 'Sending with SendGrid is Fun',
       text: 'and easy to do anywhere, even with Node.js',
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>'
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      templateId: 'd-9151449bb4e4476eb06436f9574a0a01',
+      dynamic_template_data: {
+        orderNumber: '999999',
+        eventName: 'Testing Templates',
+        purchaseNameFirst: 'Some One',
+        influencerName: 'Denver',
+        timeSlot: '12:00',
+        eventDay: '24th Dec 2018'
+      }
     };
 
     return sgMail
       .send(msg)
       .then(() => console.log('email sent'))
-      .catch(err => console.error(err));
+      .catch(error => {
+        console.error(error.toString());
+      });
   });
